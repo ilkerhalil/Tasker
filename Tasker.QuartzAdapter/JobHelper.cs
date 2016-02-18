7ed2a157308932044f532955f509f753048cf76c
Ilker Halil Turer <ilkerhalil@gmail.com>
2016-02-18 13:38:28 +0200

    Move readme.md solution directory.

---
+++ Tasker.QuartzAdapter/JobHelper.cs
@@ -0,0 +1,17 @@
+﻿using Quartz;
+using Tasker.Common.Abstraction;
+
+namespace Tasker.QuartzAdapter
+{
+    public static class JobHelper
+    {
+        public static IJob ConvertItakToIJob<T>(T task)
+            where T : ITask
+        {
+
+            //task.GetType().
+
+            return null;
+        }
+    }
+}
