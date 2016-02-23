58e80e2d6c76b8da1328cd55f82357e824a67f90
mguler <murat.guler@lycos.com>
2016-02-23 11:01:38 +0200

    Cron dökümantasyonu eklendi

--- Tasker.Common/Abstraction/IModule.cs
+++ Tasker.Common/Abstraction/IModule.cs
@@ -4,18 +4,17 @@ namespace Tasker.Common.Abstraction
 {
     /// <summary>
     /// Uygulamaya eklenti yazabilmek için implemente edilmesi gereken temel interface.
-    /// Bu interface'den türeyen tipler zamanlanmýþ bir görevi çalýþtýrabileceði gibi 
+    /// Bu interface'den türeyen tipler zamanlanmýþ bir görevi yerine getirebileceði gibi 
     /// farklý amaçlar için yazýlmýþ eklentilerde olabilirler.
     /// </summary>
     public interface IModule
     {
-        /// <summary>Modül adý</summary>
-        /// <remarks>Modülü tanýmlamak için ve diðer modüllerden ayýrabilmek için kullanýlýr</remarks>
+        /// <summary>Modül adý. Modülü tanýmlamak için ve diðer modüllerden ayýrabilmek için kullanýlýr</summary>
         /// <value><see cref="System.String"/> tipinde bir deðer.</value>
         string ModuleName { get; }
-        /// <summary>Modül parametreleri</summary>
-        /// <remarks>Modülün çalýþmak için ihtiyaç duyabileceði bilgilerin modüle aktarýlmasýný saðlayan özellik</remarks>
-        /// <value> <see cref="System.Collections.Generic.IDictionary{TKey, TValue}" /></value>
+        /// <summary>Modül parametreleri. Modülün çalýþmak için ihtiyaç duyabileceði parametrelerin modüle 
+        /// aktarýlmasýný saðlayan özellik.</summary>
+        /// <value> <see cref="System.Collections.Generic.IDictionary{TKey, TValue}" /> tipinden bir instance</value>
         IDictionary<string, object> ModuleParameters { get; }
     }
 }
--- Tasker.Common/Abstraction/TaskBase.cs
+++ Tasker.Common/Abstraction/TaskBase.cs
@@ -3,23 +3,19 @@
 namespace Tasker.Common.Abstraction
 {
     /// <summary>
-    /// Todo:Dökümantasyonu yazılmalı
+    /// Zamanlanmış görevleri yerine getirecek eklentiler yazabilmek için temel abstract sınıf 
+    /// <see cref="Tasker.Common.Abstraction.ITask"/> interface'ini implemente eder
     /// </summary>
     public abstract class TaskBase : ITask
     {
         public abstract string ModuleName { get; }
-
         public virtual IDictionary<string, object> ModuleParameters { get; }
 
         protected TaskBase()
         {
             CronPrefix = new List<string>();
             ModuleParameters = new Dictionary<string, object>();
-            
         }
-        /// <summary>
-        /// 
-        /// </summary>
         public virtual IList<string> CronPrefix { get; }
 
         public abstract string JobName { get; }
